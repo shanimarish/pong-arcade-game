bc95a3d652e1be34c205998fe043e82729138514
Shani <74494158+shanimarish@users.noreply.github.com>
2024-12-07 12:32:44 +0800

    Create another paddle

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ screen.bgcolor("black")
 screen.title("Pong Arcade Game")
 screen.tracer(0)
 
-paddle = Paddle()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(paddle.go_up, "Up")
-screen.onkey(paddle.go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
 
 game_is_on = True
 while game_is_on:
--- paddle.py
+++ paddle.py
@@ -1,18 +1,14 @@
 from turtle import Turtle
 
-X_POSITION_RIGHT = 350
-X_POSITION_LEFT = -350
-Y_POSITION = 0
-
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.shape("square")
         self.color("white")
         self.up()
         self.shapesize(stretch_len=1, stretch_wid=5)
-        self.goto(X_POSITION_RIGHT, Y_POSITION)
+        self.goto(position)
 
     def go_up(self):
         new_y = self.ycor() + 20
@@ -21,3 +17,4 @@ class Paddle(Turtle):
     def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
+
