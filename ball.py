4b94ea9a9d1cc20d83e9617f995c87545aa2b23d
Shani <74494158+shanimarish@users.noreply.github.com>
2024-12-08 22:17:44 +0800

    Implement out-of-bounds detection for the ball

--- ball.py
+++ ball.py
@@ -8,7 +8,7 @@ class Ball(Turtle):
         self.color("white")
         self.up()
         self.x_move = 10
-        self.y_move= 10
+        self.y_move = 10
 
     def move(self):
         new_x = self.xcor() + self.x_move
@@ -21,3 +21,6 @@ class Ball(Turtle):
     def bounce_x(self):
         self.x_move *= -1
 
+    def restart(self):
+        self.goto(0, 0)
+        self.bounce_x()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -31,5 +33,11 @@ while game_is_on:
 
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.restart()
+
+    if ball.xcor() < -380:
+        ball.restart()
 
 screen.exitonclick()
